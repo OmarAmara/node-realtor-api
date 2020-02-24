8badc8807759f2cd9729310b41cf075e75353579
Omar Amara <O_amara@hotmail.com>
2020-02-24 11:24:47 -0600

    Server set-up

---
+++ server.js
@@ -0,0 +1,27 @@
+// MODULES --
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+// make Socket.io a different PORT
+const PORT = process.env.PORT
+
+
+// MIDDLEWARE --
+
+
+// CONTROLLERS --
+
+
+
+// ROUTES --
+
+
+// LISTENER --
+
+app.listen(PORT, () => {
+	const d = new Date
+	console.log(`\n${d} Server running on port: ${PORT}`);
+})
+
+
