6431a30aa89ab6e398ce51c522e1aab25cb703c6
Omar Amara <O_amara@hotmail.com>
2020-02-24 13:24:10 -0600

    Able to send and receive json data from post route

--- controllers/clientAuthController.js
+++ controllers/clientAuthController.js
@@ -7,4 +7,18 @@ router.get('/', async (req, res, next) => {
 	res.json(data={ greet: 'hello' })
 })
 
+router.post('/', async (req, res, next) => {
+	try{
+		// const desiredEmail = req.body.email
+		// const desiredUsername = req.body.username
+		// const desiredPassword = req.body.password
+		console.log('client post route hit');
+		console.log(req.body);
+		res.json(req.body.name + " is a good name")
+	} catch(err) {
+		// try creating custom error --> (console.error) or respond with if status ===... res.json(...)
+		next(err)
+	}
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// MODULES --
+/* -- MODULES -- */
 require('dotenv').config()
 require('./db/db.js')
 const express = require('express')
@@ -7,25 +7,29 @@ const app = express()
 // make Socket.io a different PORT
 const PORT = process.env.PORT
 
+// middleware modules
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
 
-// MIDDLEWARE --
+
+/* -- MIDDLEWARE -- */
+app.use(bodyParser.urlencoded({ extended: false })) // request.body
+app.use(methodOverride('_method')) // necessary?
+app.use(express.json()) // utilize json data from request.body
 
 
-// CONTROLLERS --
+/* -- CONTROLLERS -- */
 const clientAuthController = require('./controllers/clientAuthController')
 app.use('/api/v1.0/clients', clientAuthController)
 
 
-// ROUTES --
+/* -- ROUTES -- */
 app.get('*', (req, res) => {
 	res.status(404).send('404 NOT FOUND')	
 })
 
-// LISTENER --
-
+/* -- LISTENER -- */
 app.listen(PORT, () => {
 	const d = new Date
 	console.log(`\n${d} Server running on port: ${PORT}`);
 })
-
-
